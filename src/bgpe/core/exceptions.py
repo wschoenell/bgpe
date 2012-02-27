8bd0fa6b6a9eb9206f4915141a0304b377890217
william <wschoenell@gmail.com>
2012-02-27 03:08:24 +0100

    Initial version

--- src/bgpe/core/exceptions.py
+++ src/bgpe/core/exceptions.py
@@ -3,3 +3,46 @@ Created on Feb 23, 2012
 
 @author: william
 '''
+
+import sys
+import traceback
+
+#import logging
+
+def printException (e, stream=sys.stdout):
+
+    print >> stream, ''.join(strException(e))
+
+    if hasattr(e, 'cause') and getattr(e, 'cause') != None:
+        print >> stream, "Caused by:",
+        print >> stream, ''.join(e.cause)
+
+def strException (e):
+
+    # Adapted from chimera observatory control system
+    #      (http://code.google.com/p/chimera)
+    try:
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        local_tb = traceback.format_exception(exc_type, exc_value, exc_tb)
+        return local_tb
+    finally:
+        # clean up cycle to traceback, to allow proper GC
+        del exc_type, exc_value, exc_tb
+
+#    Exceptions Hierarchy
+
+class BGPEException(Exception):
+
+    def __init__ (self, msg="", *args):
+        Exception.__init__ (self, msg, *args)
+
+        if not all(sys.exc_info()):
+            self.cause = None
+        else:
+            self.cause = strException(sys.exc_info()[1])
+            
+class HDF5dbException(BGPEException):
+    pass
+
+class ReadFilterException(BGPEException):
+    pass
