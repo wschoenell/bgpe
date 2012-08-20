56385558ed244d3a53f04173ed472f90c4aae248
William Schoenell <wschoenell@gmail.com>
2012-08-20 20:51:49 +0200

    io.readfilterset: added readfilterset.uniform. It interpolates the filtercurve to uniform it on a determined dl interval.

--- src/bgpe/io/readfilterset.py
+++ src/bgpe/io/readfilterset.py
@@ -17,14 +17,31 @@ class readfilterset(object):
     def __init__(self):
         pass
     
-    def read(self, filterfile):        
+    def read(self, filterfile):  
+        ''' Reads filterfile '''      
         try:
             dt = np.dtype ([('ID_filter', 'S20'), ('wl', 'f'), ('transm', 'f')])
             self.filterset = np.loadtxt(filterfile, dtype=dt)
         except:
             raise ReadFilterException('Cannot read filterfile %s' % filterfile)
         
+    def uniform(self, dl=1):
+        ''' Interpolates filter curves to match a specific uniform lambda coverage
+            Argument:
+            dl: Delta lambda spacing (default: 1 Angstrom)
+        '''
+        aux = []
+        for fid in np.unique(self.filterset['ID_filter']):
+            xx = self.filterset[self.filterset['ID_filter'] == fid]
+            new_lambda = np.arange(xx['wl'].min(), xx['wl'].max(), 1.0)
+            new_transm = np.interp(new_lambda, xx['wl'], xx['transm'])
+            for i in range(len(new_lambda)):
+                aux.append((fid, new_lambda[i], new_transm[i]))
+        self.filterset = np.array(aux, dtype = self.filterset.dtype)
+            
+        
     def calc_filteravgwls(self):
+        ''' Calulates the mean wavelenght of each filter (good for plotting)'''
         avg = []
         for fid in np.unique(self.filterset['ID_filter']):
             avg.append(np.average(self.filterset[self.filterset['ID_filter'] == fid]['wl']))
